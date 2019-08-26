984248ed683a4054b3e4d8f8f8996f58ec88f70d
DevinChang <devinchang@126.com>
2019-08-26 21:16:35 +0800

    add readme

---
+++ cmake-build-debug/CMakeFiles/WritenExamination.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/devinchang/Code/WritenExamination/MeiTuan8_22.cpp" "/home/devinchang/Code/WritenExamination/cmake-build-debug/CMakeFiles/WritenExamination.dir/MeiTuan8_22.cpp.o"
+  "/home/devinchang/Code/WritenExamination/Yuan824.cpp" "/home/devinchang/Code/WritenExamination/cmake-build-debug/CMakeFiles/WritenExamination.dir/Yuan824.cpp.o"
+  "/home/devinchang/Code/WritenExamination/main.cpp" "/home/devinchang/Code/WritenExamination/cmake-build-debug/CMakeFiles/WritenExamination.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/WritenExamination.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WritenExamination.dir/MeiTuan8_22.cpp.o"
+  "CMakeFiles/WritenExamination.dir/main.cpp.o"
+  "CMakeFiles/WritenExamination.dir/Yuan824.cpp.o"
+  "WritenExamination.pdb"
+  "WritenExamination"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WritenExamination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
